9cf0b38f124bfa49304ee4e22c9695bf645d2948
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-03 18:45:24 +0300

    2.2.2. Добавление бинов в контекст Spring с помощью стереотипных аннотаций

--- chapter-02/sq-ch02-ex06/src/main/java/main/Parrot.java
+++ chapter-02/sq-ch02-ex06/src/main/java/main/Parrot.java
@@ -1,11 +1,16 @@
 package main;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Parrot {
     private String name;
 
+    @PostConstruct
+    public void init() {
+        this.name = "Kiki";
+    }
     public String getName() {
         return name;
     }
