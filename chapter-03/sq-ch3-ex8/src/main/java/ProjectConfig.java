ef4097b2a1a886764d3633d3b08e78121a0f9ef5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-04 11:52:12 +0300

    3.4. Выбор из нескольких бинов в контексте Spring

---
+++ chapter-03/sq-ch3-ex8/src/main/java/ProjectConfig.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Parrot parrot1() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Koko");
+        return parrot;
+    }
+
+    @Bean
+    public Parrot parrot2() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Miki");
+        return parrot;
+    }
+
+    @Bean
+    public Person person(@Qualifier("parrot2") Parrot parrot){
+        Person person = new Person();
+        person.setName("Ella");
+        person.setParrot(parrot);
+        return person;
+    }
+}
