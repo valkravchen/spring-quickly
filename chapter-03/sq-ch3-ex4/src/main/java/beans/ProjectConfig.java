7470d791500a57fab30975e1157cf5c18f600dec
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-04 08:24:50 +0300

    3.2.1. Внедрение значений через поля класса с использованием аннотации @Autowired

---
+++ chapter-03/sq-ch3-ex4/src/main/java/beans/ProjectConfig.java
@@ -0,0 +1,9 @@
+package beans;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "beans")
+public class ProjectConfig {
+}
