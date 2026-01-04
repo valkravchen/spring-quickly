c709ad2d22d75d4615b4bd2ff6bf38163bbfdd70
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-04 20:20:14 +0300

    Глава 04, пример 03: внедрение зависимостей с помощью Spring @Component и @Autowired

---
+++ chapter-04/sq-ch4-ex3/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import proxies.CommentNotificationProxy;
+import proxies.EmailCommentNotificationProxy;
+import repositories.CommentRepository;
+import repositories.DBCommentRepository;
+import services.CommentService;
+
+@Configuration
+@ComponentScan(basePackages = {"proxies", "repositories", "services"})
+public class ProjectConfig {
+}
