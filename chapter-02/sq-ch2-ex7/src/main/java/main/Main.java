4d77de54b1ae48c4ad247ffe3d0eda2bdb42f41d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-05 22:34:25 +0300

    Добавление бинов в контекст Spring с помощью стереотипных аннотаций

---
+++ chapter-02/sq-ch2-ex7/src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    static void main() {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot parrot = context.getBean(Parrot.class);
+        System.out.println(parrot);
+        System.out.println(parrot.getName());
+    }
+}
+
