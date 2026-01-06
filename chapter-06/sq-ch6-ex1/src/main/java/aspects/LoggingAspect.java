5a9091b9d69026b33a75262180567a2256b6ea23
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-06 22:30:16 +0300

    Практическое применение прототипных бинов

---
+++ chapter-06/sq-ch6-ex1/src/main/java/aspects/LoggingAspect.java
@@ -0,0 +1,21 @@
+package aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.logging.Logger;
+
+@Aspect
+public class LoggingAspect {
+
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    @Around("execution(* services.*.*(..))")
+    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Метод будет выполнен");
+        joinPoint.proceed();
+        logger.info("Метод выполнен");
+    }
+}
+
