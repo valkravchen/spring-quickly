d29f9977c2a859eab8c8c2c77cf7f49e9738b542
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-06 15:56:41 +0300

    Реализация сценариев использования без применения фреймворка

---
+++ chapter-04/sq-ch4-ex2/src/main/java/proxies/EmailCommentNotificationProxy.java
@@ -0,0 +1,12 @@
+package proxies;
+
+import model.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailCommentNotificationProxy implements CommentNotificationProxy {
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Отправка уведомления для получения комментариев: " + comment.getText());
+    }
+}
