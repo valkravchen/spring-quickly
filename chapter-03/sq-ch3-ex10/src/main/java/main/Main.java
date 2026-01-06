f8df21a946d607d7b9ee5a313a5648aacbf25cfd
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-06 11:06:49 +0300

    Выбор из нескольких бинов в контексте Spring

---
+++ chapter-03/sq-ch3-ex10/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import beans.Person;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        System.out.println("beans.Person's name: " + person.getName());
+        System.out.println("beans.Person's parrot: " + person.getParrot());
+    }
+}
+
