dd42c1b2bde0ba9f01239c0c701a2ba8e410b89c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-07 17:13:12 +0300

    Передача данных от клиента серверу посредством параметров запроса

---
+++ chapter-08/sq-ch8-ex2/src/main/java/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @RequestMapping("/home")
+    public String home(@RequestParam String color, Model page) {
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", color);
+        return  "home";
+    }
+}
