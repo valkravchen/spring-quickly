4227612723867f67010d2839c78b98069c78a3da
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-08 19:56:06 +0300

    Передача данных от клиента серверу с помощью переменных пути

---
+++ chapter-09/sq-ch9-ex3/src/main/java/com/service/LoginCountService.java
@@ -0,0 +1,18 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@Service
+@ApplicationScope
+public class LoginCountService {
+    private int count;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
