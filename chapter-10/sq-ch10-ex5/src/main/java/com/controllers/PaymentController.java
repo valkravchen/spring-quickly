2f33ff0dc8627b4688dc82cc648c04b0f12255ef
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-09 10:49:54 +0300

    Управление исключениями на уровне конечной точки

---
+++ chapter-10/sq-ch10-ex5/src/main/java/com/controllers/PaymentController.java
@@ -0,0 +1,35 @@
+package com.controllers;
+
+import com.exceptions.NotEnoughMoneyException;
+import com.model.ErrorDetails;
+import com.model.PaymentDetails;
+import com.service.PaymentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payment")
+    public ResponseEntity<?> makePayment() {
+        try {
+            PaymentDetails paymentDetails = paymentService.processPayment();
+            return ResponseEntity
+                    .status(HttpStatus.ACCEPTED)
+                    .body(paymentDetails);
+        } catch (NotEnoughMoneyException e) {
+            ErrorDetails errorDetails = new ErrorDetails();
+            errorDetails.setMessage("Недостаточно средств для совершения платежа.");
+            return ResponseEntity
+                    .badRequest()
+                    .body(errorDetails);
+        }
+    }
+}
