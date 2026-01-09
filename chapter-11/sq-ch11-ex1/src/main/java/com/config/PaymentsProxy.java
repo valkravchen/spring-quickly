55fd270ef080b003985777e39203225134a90ce1
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-09 16:59:05 +0300

    Управление исключениями на уровне конечной точки

---
+++ chapter-11/sq-ch11-ex1/src/main/java/com/config/PaymentsProxy.java
@@ -0,0 +1,9 @@
+package com.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.proxy")
+public class PaymentsProxy {
+}
---
+++ chapter-11/sq-ch11-payments/src/main/java/com/controllers/PaymentsController.java
@@ -0,0 +1,31 @@
+package com.controllers;
+
+import com.model.Payment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@RestController
+public class PaymentsController {
+    private static Logger logger = Logger.getLogger(PaymentsController.class.getName());
+
+    @PostMapping("/payment")
+    public ResponseEntity<Payment> createPayment(
+            @RequestHeader String requestId,
+            @RequestBody Payment payment
+    ) {
+        logger.info("Получен запрос с идентификатором " + requestId +
+                " ; Сумма платежа: " + payment.getAmount());
+        payment.setId(UUID.randomUUID().toString());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("requestId", requestId)
+                .body(payment);
+    }
+}
