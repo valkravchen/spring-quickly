9dc9f780ddcca20a244122c1e51719d70ec14003
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-09 18:33:16 +0300

    Управление исключениями на уровне конечной точки

---
+++ chapter-11/sq-ch11-ex2/src/main/java/com/controllers/PaymentsController.java
@@ -0,0 +1,23 @@
+package com.controllers;
+
+import com.model.Payment;
+import com.proxy.PaymentsProxy;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentsController {
+    private final PaymentsProxy paymentsProxy;
+
+    public PaymentsController(PaymentsProxy paymentsProxy) {
+        this.paymentsProxy = paymentsProxy;
+    }
+
+    @PostMapping("/payment")
+    public Payment createPayment(@RequestBody Payment payment) {
+        return paymentsProxy.createPayment(payment);
+    }
+}
