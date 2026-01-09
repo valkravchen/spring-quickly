e024b8fcb34d3a24a19f803cef299331072b2504
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-01-09 08:57:29 +0300

    f

---
+++ chapter-09/sq-ch9-ex3/src/main/java/com/controllers/MainController.java
@@ -0,0 +1,36 @@
+package com.controllers;
+
+import com.service.LoggedUserManagementService;
+import com.service.LoginCountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    public MainController(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    @GetMapping("/main")
+    public String home(
+            @RequestParam(required = false) String logout,
+            Model model
+    ) {
+        if (logout != null) {
+            loggedUserManagementService.setUsername(null);
+        }
+        String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
+        if (username == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("username", username);
+        return "main.html";
+    }
+}
